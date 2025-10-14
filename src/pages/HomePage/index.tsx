a83cb9ff1905e50a422e7218a0a2d23b07efcf4a
Do Truong Thinh <dotruongthinh2212@gmail.com>
2025-10-14 17:30:20 +0700

    redesign home page

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,21 +1,21 @@
 import Navbar from '../../components/shared/navbar';
-import { HeroBanner } from './components/hero-banner';
-import { CategoryGrid } from './components/category-grid';
-import { ProductCarousel } from './components/product-carousel';
-import { DealsSection } from './components/deals-section';
 import { Footer } from './components/footer';
+import { LiteratureHero } from './components/literature-hero';
+import { Features } from './components/features';
+import { HowItWorks } from './components/how-it-works';
+import { Testimonials } from './components/testimonials';
+import { CallToAction } from './components/call-to-action';
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
       <main>
-        <HeroBanner />
-        <CategoryGrid />
-        <ProductCarousel title="Best Sellers" />
-        <DealsSection />
-        <ProductCarousel title="Recommended for You" />
-        <ProductCarousel title="Recently Viewed" />
+        <LiteratureHero />
+        <Features />
+        <HowItWorks />
+        <Testimonials />
+        <CallToAction />
       </main>
       <Footer />
     </div>
